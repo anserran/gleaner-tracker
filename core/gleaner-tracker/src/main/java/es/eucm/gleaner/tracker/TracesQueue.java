8f1618212a64e1eb4c6e9e25d657bffc55fab6f0
RotaruDan <drotaru@ucm.es>
2015-08-26 10:35:35 +0200

    TracesQueue uses TracesConversor to transform traces to xAPI statements.

--- core/gleaner-tracker/src/main/java/es/eucm/gleaner/tracker/TracesQueue.java
+++ core/gleaner-tracker/src/main/java/es/eucm/gleaner/tracker/TracesQueue.java
@@ -1,189 +1,200 @@
 package es.eucm.gleaner.tracker;
 
+import es.eucm.gleaner.tracker.conversor.TracesConversor;
 import es.eucm.gleaner.tracker.model.TrackData;
+import es.eucm.gleaner.tracker.model.traces.Events;
+import es.eucm.gleaner.tracker.model.traces.InputTrace;
+import es.eucm.gleaner.tracker.model.traces.LogicTrace;
 import es.eucm.gleaner.tracker.model.traces.Trace;
 import es.eucm.gleaner.network.Header;
 import es.eucm.gleaner.network.requests.Request;
 import es.eucm.gleaner.network.requests.RequestCallback;
 import es.eucm.gleaner.network.requests.RequestHelper;
 import es.eucm.gleaner.network.requests.Response;
+import es.eucm.gleaner.tracker.model.traces.xapi.*;
 
+import java.lang.Object;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TracesQueue implements RequestCallback {
 
-	private final RequestHelper requestHelper;
+    private final RequestHelper requestHelper;
 
-	/**
-	 * A request callback interested on the request results of the queue
-	 */
-	private final RequestCallback requestCallback;
+    /**
+     * A request callback interested on the request results of the queue
+     */
+    private final RequestCallback requestCallback;
 
-	/**
-	 * Track data
-	 */
-	private TrackData trackData;
+    /**
+     * Track data
+     */
+    private TrackData trackData;
 
-	/**
-	 * Url where to send the traces
-	 */
-	private String url;
+    /**
+     * Url where to send the traces
+     */
+    private String url;
 
-	/**
-	 * Stored traces. Some might be sent to the server, others might not
-	 */
-	private List<Trace> storedTraces;
+    /**
+     * Stored traces. Some might be sent to the server, others might not
+     */
+    private List<Trace> storedTraces;
 
-	/**
-	 * Aux list to add the traces to send in the next batch
-	 */
-	private List<Trace> tracesToSend;
+    /**
+     * Aux list to add the traces to send in the next batch
+     */
+    private List<Trace> tracesToSend;
 
-	/**
-	 * Index of the last trace sent in storedTraces list
-	 */
-	private int lastTraceSentIndex;
+    /**
+     * Converts traces to xAPI statements.
+     */
+    private TracesConversor conversor;
 
-	/**
-	 * Index of the last trace sent to the server that has been positively
-	 * acknowledged by the server
-	 */
-	private int lastTraceReceivedIndex;
+    /**
+     * Index of the last trace sent in storedTraces list
+     */
+    private int lastTraceSentIndex;
 
-	/**
-	 * This flag is true when we're waiting for a server's response
-	 */
-	private boolean sending;
+    /**
+     * Index of the last trace sent to the server that has been positively
+     * acknowledged by the server
+     */
+    private int lastTraceReceivedIndex;
 
-	/**
-	 * Max size
-	 */
-	private int maxSize;
+    /**
+     * This flag is true when we're waiting for a server's response
+     */
+    private boolean sending;
 
-	/**
-	 * Current max size
-	 */
-	private int currentMaxSize;
+    /**
+     * Max size
+     */
+    private int maxSize;
 
-	public TracesQueue(RequestHelper requestHelper,
-			RequestCallback requestCallback) {
-		this.requestHelper = requestHelper;
-		this.requestCallback = requestCallback;
-		this.storedTraces = new ArrayList<Trace>();
-		this.tracesToSend = new ArrayList<Trace>();
-		this.lastTraceSentIndex = -1;
-		this.lastTraceReceivedIndex = -1;
-		this.maxSize = -1;
-		this.currentMaxSize = maxSize;
-		this.sending = false;
-	}
+    /**
+     * Current max size
+     */
+    private int currentMaxSize;
 
-	/**
-	 * @return true of the tracker has started a session in the server
-	 */
-	public boolean isConnected() {
-		return trackData != null;
-	}
+    public TracesQueue(RequestHelper requestHelper,
+                       RequestCallback requestCallback) {
+        this.requestHelper = requestHelper;
+        this.requestCallback = requestCallback;
+        this.storedTraces = new ArrayList<Trace>();
+        this.tracesToSend = new ArrayList<Trace>();
+        this.conversor = new TracesConversor();
+        this.lastTraceSentIndex = -1;
+        this.lastTraceReceivedIndex = -1;
+        this.maxSize = -1;
+        this.currentMaxSize = maxSize;
+        this.sending = false;
+    }
 
-	/**
-	 * Sets the number of traces that must be stored without sending them to the
-	 * server. When the number of traces exceeds this number, a flush is
-	 * automatically triggered. If maxSize == -1, then traces are not sent
-	 * automatically
-	 * 
-	 * @param maxSize
-	 *            max size
-	 */
-	public void setMaxSize(int maxSize) {
-		this.maxSize = maxSize;
-		this.currentMaxSize = maxSize;
-	}
+    /**
+     * @return true of the tracker has started a session in the server
+     */
+    public boolean isConnected() {
+        return trackData != null;
+    }
 
-	/**
-	 * Sets the url to post the traces
-	 * 
-	 * @param url
-	 *            the url
-	 */
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    /**
+     * Sets the number of traces that must be stored without sending them to the
+     * server. When the number of traces exceeds this number, a flush is
+     * automatically triggered. If maxSize == -1, then traces are not sent
+     * automatically
+     *
+     * @param maxSize max size
+     */
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+        this.currentMaxSize = maxSize;
+    }
 
-	/**
-	 * Add trace
-	 * 
-	 * @param trace
-	 *            the trace to add
-	 */
-	public synchronized void add(Trace trace) {
-		storedTraces.add(trace);
-		if (currentMaxSize >= 0
-				&& storedTraces.size() - lastTraceReceivedIndex > currentMaxSize) {
-			flush();
-		}
-	}
+    /**
+     * Sets the url to post the traces
+     *
+     * @param url the url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	/**
-	 * Clears the traces, without sending them to the server
-	 */
-	public void clear() {
-		storedTraces.clear();
-		tracesToSend.clear();
-		lastTraceSentIndex = -1;
-		lastTraceReceivedIndex = -1;
-		currentMaxSize = maxSize;
-	}
+    /**
+     * Add trace
+     *
+     * @param trace the trace to add
+     */
+    public synchronized void add(Trace trace) {
+        storedTraces.add(trace);
+        if (currentMaxSize >= 0
+                && storedTraces.size() - lastTraceReceivedIndex > currentMaxSize) {
+            flush();
+        }
+    }
 
-	/**
-	 * Clears the traces, sending them to the server
-	 */
-	public void flush() {
-		if (storedTraces.size() > 0
-				&& lastTraceReceivedIndex < storedTraces.size()
-				&& isConnected() && !sending) {
-			sending = true;
-			tracesToSend.clear();
-			for (int i = lastTraceReceivedIndex + 1; i < storedTraces.size(); i++) {
-				tracesToSend.add(storedTraces.get(i));
-			}
-			lastTraceSentIndex = storedTraces.size() - 1;
-			requestHelper.url(url)
-					.header(Header.AUTHORIZATION, trackData.getAuthToken())
-					.post(tracesToSend, this);
-		}
-	}
+    /**
+     * Clears the traces, without sending them to the server
+     */
+    public void clear() {
+        storedTraces.clear();
+        tracesToSend.clear();
+        lastTraceSentIndex = -1;
+        lastTraceReceivedIndex = -1;
+        currentMaxSize = maxSize;
+    }
 
-	public void setTrackData(TrackData trackData) {
-		this.trackData = trackData;
-	}
+    /**
+     * Clears the traces, sending them to the server
+     */
+    public void flush() {
+        if (storedTraces.size() > 0
+                && lastTraceReceivedIndex < storedTraces.size()
+                && isConnected() && !sending) {
+            sending = true;
+            tracesToSend.clear();
+            for (int i = lastTraceReceivedIndex + 1; i < storedTraces.size(); i++) {
+                tracesToSend.add(storedTraces.get(i));
+            }
+            lastTraceSentIndex = storedTraces.size() - 1;
+            requestHelper.url(url)
+                    .header(Header.AUTHORIZATION2, trackData.getAuthToken())
+                    .post(conversor.convert(tracesToSend,trackData.getPlayerName()), this);
+        }
+    }
 
-	@Override
-	public void error(Request request, Throwable throwable) {
-		sending = false;
-		if (currentMaxSize > 0) {
-			// Increase the windows' size, to avoid too much attempts
-			currentMaxSize += maxSize;
-		}
-		requestCallback.error(request, throwable);
-	}
+    public void setTrackData(TrackData trackData) {
+        this.trackData = trackData;
+    }
 
-	@Override
-	public void success(Request request, Response response) {
-		sending = false;
-		if (response.getStatusCode() == Response.OK
-				|| response.getStatusCode() == Response.NO_CONTENT) {
-			currentMaxSize = maxSize;
-			lastTraceReceivedIndex = lastTraceSentIndex;
-		}
-		requestCallback.success(request, response);
-	}
+    @Override
+    public void error(Request request, Throwable throwable) {
+        sending = false;
+        if (currentMaxSize > 0) {
+            // Increase the windows' size, to avoid too much attempts
+            currentMaxSize += maxSize;
+        }
+        requestCallback.error(request, throwable);
+    }
 
-	public int size() {
-		return storedTraces.size() - (lastTraceReceivedIndex + 1);
-	}
+    @Override
+    public void success(Request request, Response response) {
+        sending = false;
+        if (response.getStatusCode() == Response.OK
+                || response.getStatusCode() == Response.NO_CONTENT) {
+            currentMaxSize = maxSize;
+            lastTraceReceivedIndex = lastTraceSentIndex;
+        }
+        requestCallback.success(request, response);
+    }
 
-	public boolean isDone() {
-		return lastTraceReceivedIndex == storedTraces.size() - 1;
-	}
+    public int size() {
+        return storedTraces.size() - (lastTraceReceivedIndex + 1);
+    }
+
+    public boolean isDone() {
+        return lastTraceReceivedIndex == storedTraces.size() - 1;
+    }
 }
