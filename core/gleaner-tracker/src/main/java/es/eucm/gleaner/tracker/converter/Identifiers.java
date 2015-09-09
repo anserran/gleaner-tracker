9702473a09be97cf86ffa041c42f2930726346d5
RotaruDan <drotaru@ucm.es>
2015-09-09 11:24:25 +0200

    Adds TracesConverter.

---
+++ core/gleaner-tracker/src/main/java/es/eucm/gleaner/tracker/converter/Identifiers.java
@@ -0,0 +1,11 @@
+package es.eucm.gleaner.tracker.converter;
+
+public interface Identifiers {
+
+	String BASE_ID = "http://www.gleaner.com/";
+	String OBJECTS_ID = BASE_ID + "objects/";
+	String GAMES_ID = BASE_ID + "games/lostinspace/";
+
+	String ACTIVITY_OBJECT_TYPE = "Activity";
+	String OBJECT_NONE = "none";
+}
---
+++ core/gleaner-tracker/src/main/java/es/eucm/gleaner/tracker/converter/InputTraceConverter.java
@@ -0,0 +1,68 @@
+package es.eucm.gleaner.tracker.converter;
+
+import es.eucm.gleaner.tracker.model.traces.Events;
+import es.eucm.gleaner.tracker.model.traces.InputTrace;
+import es.eucm.gleaner.tracker.model.traces.xapi.*;
+
+import java.lang.Object;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Transforms {@link InputTrace}s into {@link XAPIObject}s.
+ */
+public class InputTraceConverter implements
+		TracesConverter.TraceConversor<InputTrace> {
+
+	@Override
+	public XAPIObject convert(InputTrace trace, String playerName) {
+		String event = trace.getEvent();
+		String verbString = Events.toVerb.get(event);
+
+		if (verbString == null) {
+			verbString = event;
+		}
+
+		String objectId = trace.getTarget();
+		if (objectId == null) {
+			objectId = Identifiers.OBJECT_NONE;
+		}
+
+		String verbId = verbString, definitionType = objectId;
+
+		Map<String, Object> extensions = new HashMap<String, Object>();
+		extensions.put("event", event);
+		if (trace.getTarget() != null) {
+			extensions.put("target", trace.getTarget());
+		}
+		if (trace.getValue1() != null) {
+			extensions.put("value1", trace.getValue1());
+		}
+		if (trace.getValue2() != null) {
+			extensions.put("value2", trace.getValue2());
+		}
+		if (trace.getValue3() != null) {
+			extensions.put("value3", trace.getValue3());
+		}
+
+		// Actor
+		Actor actor = new Actor(new Account(playerName), playerName);
+
+		// Verb
+		Map<String, String> display = new HashMap<String, String>();
+		display.put("en-US", verbId);
+		display.put("es-ES", verbId);
+		Verb verb = new Verb(Identifiers.BASE_ID + verbId, display);
+
+		// Object
+		Definition definition;
+		es.eucm.gleaner.tracker.model.traces.xapi.Object object;
+		definition = new Definition(Identifiers.OBJECTS_ID + definitionType,
+				extensions);
+
+		object = new es.eucm.gleaner.tracker.model.traces.xapi.Object(
+				Identifiers.GAMES_ID + objectId, definition);
+
+		return new XAPIObject(actor, verb, object);
+	}
+}
